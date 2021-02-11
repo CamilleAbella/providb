a0fda7c774dfaa1fd0b7d03ae2afa81454176fc9
CamilleAbella <camille.abella@hotmail.fr>
2021-02-11 12:18:38 +0100

    implement abstract provider

--- src/app/provider.ts
+++ src/app/provider.ts
@@ -1,1 +1,77 @@
-export abstract class Provider {}
+/**
+ *
+ */
+export interface ProviderConfig {}
+
+/**
+ *
+ */
+export abstract class Provider<
+  Value extends boolean | number | string = boolean | number | string
+> {
+  /**
+   *
+   * @param key
+   */
+  abstract delete(key: string): Promise<this>
+
+  /**
+   *
+   */
+  abstract deleteAll(): Promise<this>
+
+  /**
+   *
+   * @param key
+   */
+  abstract get<value extends Value = Value>(key: string): Promise<value>
+
+  /**
+   *
+   * @param key
+   * @param value
+   */
+  abstract set<value extends Value = Value>(
+    key: string,
+    value: value
+  ): Promise<this>
+
+  /**
+   *
+   * @param key
+   * @param defaultValue
+   */
+  abstract ensure<value extends Value = Value>(
+    key: string,
+    defaultValue: value
+  ): Promise<value>
+
+  /**
+   *
+   * @param test
+   */
+  abstract find<value extends Value = Value>(
+    test: (value: Value, key: string) => boolean
+  ): Promise<value | null>
+
+  /**
+   *
+   * @param forEach
+   */
+  abstract each(forEach: (value: Value, key: string) => unknown): Promise<this>
+
+  /**
+   *
+   * @param key
+   * @param forEach
+   * @deprecated
+   */
+  abstract eachOf<
+    value extends Value = Value,
+    SubKey extends string | number = string | number,
+    SubValue extends Value = Value
+  >(
+    key: string,
+    forEach: (value: SubValue, key: SubKey) => unknown
+  ): Promise<value>
+}
